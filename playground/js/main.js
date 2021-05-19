e6c59586efa4c6d9a3dcb354adf1076111aa0a94
Freddy Polania <fpolania@gmail.com>
2021-05-19 19:18:40 -0400

    staggerFrom and staggerTo

--- playground/js/main.js
+++ playground/js/main.js
@@ -2,8 +2,10 @@
 TweenMax.to('.square', 0.75, { x: 100, delay: 0.75 })
 TweenMax.to('.rectangle', 0.75, { x: 100, delay: 1.5 }) */
 
-let tlanimation = new TimelineMax();
+/* let tlanimation = new TimelineMax();
 
-tlanimation.to('.circle', 2, { x: 100 })
-.to('.square', 0.5, { x: 100 })
-.to('.rectangle', 1, { x: 100 });
+tlanimation.to('.circle', 1, { x: 100, ease: Bounce.easeOut })
+.to('.square', 1, { x: 100, ease: Bounce.easeOut})
+.to('.rectangle', 1, { x: 100, ease: Bounce.easeOut }); */
+
+TweenMax.staggerFrom('.triangle', 1, { x: 200, y: 200, opacity: 0, ease:Back.easeOut }, 0.1);
